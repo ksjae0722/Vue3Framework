52d686a2de1d6cda038e7be0015f3f093efcaf48
ksjae <ksjae0722@naver.com>
2024-09-01 23:32:11 +0900

    [김상재] 첫 커밋

---
+++ src/main/java/Vue3Framework/Vue3FrameworkApplication.java
@@ -0,0 +1,11 @@
+package Vue3Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue3FrameworkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Vue3FrameworkApplication.class, args);
+	}
+}
---
+++ src/test/java/Vue3Framework/Vue3FrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package Vue3Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue3FrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
